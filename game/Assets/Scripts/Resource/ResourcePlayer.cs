0e41b5e72ea533b4b172ce5808ce8f9d27f81c8d
Nukio <NukiXm@yandex.ru>
2023-01-29 01:41:54 +0300

    update

---
+++ game/Assets/Scripts/Resource/ResourcePlayer.cs
@@ -0,0 +1,11 @@
+
+namespace ResourcePlayer
+{
+    public class ResourcePlayer
+    {
+        public int Level;
+        public int Xp;
+        public int Money;
+
+    }
+}
--- game/Assets/Scripts/UI/Rooms/Room_set.cs
+++ game/Assets/Scripts/UI/Rooms/Room_set.cs
@@ -1,7 +1,6 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Player_game;
 using TMPro;
 
 public class Room_set : MonoBehaviour
@@ -9,18 +8,6 @@ public class Room_set : MonoBehaviour
     [SerializeField] private TMP_Text _Level;
     [SerializeField] private TMP_Text _XP;
     [SerializeField] private TMP_Text _Money;
-    private void Start()
-    {
-        Player_game.test player_res = new test();
-        player_res.lvl = 4;
-        player_res.xp = 200;
-        player_res.money = 1337;
-        
-        _Level.text = player_res.lvl.ToString();
-        _XP.text = player_res.xp.ToString();
-        _Money.text = player_res.money.ToString();
-    }
-
     public void ScenLoad(string name)
     {
         SceneManager.LoadScene(name);
